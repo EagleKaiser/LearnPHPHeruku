13ff802817e7f42ed4ca873d93d1a07fd058f0a2
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 07:25:28 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
-    $quote = "There is no great genius without a mixture of madness.";
-    $length = strlen($quote);
-    echo $length;
-    //output: 54
+    $quote = "Courage is resistance to fear, mastery of fear, no absence of fear.";
+    echo strpos($quote, "fear")."<br>";
+    echo strpos($quote, "fear",26)."<br>";
+    echo strpos($quote, "c")."<br>";
+    echo strpos($quote, "C")."<br>";
+    echo strpos($quote, "z");
     ?>
