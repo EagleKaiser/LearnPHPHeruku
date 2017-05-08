b4c804375d21deb2106257fd45d2c54cd9913e7c
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 08:20:36 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,7 +1,8 @@
 <?php
     $quote = "Hello world";
     $varArray = str_split($quote);
-    Print_r($varArray)."<br>";
+    Print_r($varArray);
+    echo "<br>";
     $varArray = str_split($quote, 3);
     Print_r($varArray);
     ?>
