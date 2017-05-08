e205839ebd1854a5419e20f51663c7808e02423a
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 08:15:00 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,7 +1,5 @@
 <?php
-    $quote = "Only those who will risk going too far can possibly find out how far one can go.";
-    echo substr($quote, 3)."<br>";
-    echo substr($quote, -5)."<br>";
-    echo substr($quote, 3, 5)."<br>";
-    echo substr($quote, 3, -5)."<br>";
+    $quote = "Hello world";
+    $varArray = str_split($quote);
+    Print_r($varArray);
     ?>
