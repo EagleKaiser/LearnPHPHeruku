892dbd8f02e245feff82148eb89aa817a9db33df
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 08:04:42 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php
-    $quote = "Only those who will risk going too far can possibly find out how far one can go.<br>";
-    echo substr($quote, 3);
-    echo substr($quote, -5);
-    echo substr($quote, 3, 5);
-    echo substr($quote, 3, -5);
+    $quote = "Only those who will risk going too far can possibly find out how far one can go.";
+    echo substr($quote, 3)."<br>";
+    echo substr($quote, -5)."<br>";
+    echo substr($quote, 3, 5)."<br>";
+    echo substr($quote, 3, -5)."<br>";
     ?>
