81632f4ee1e50e472c84e0e3216eedc1c314659a
EagleKaiser <eagle2117652@outlook.com>
2017-05-07 22:54:44 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,14 +1,6 @@
 <?php
-    $message = "Hello World!";
-    $title = "HTML Sample";
-    ?>
-
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>PHP Fundamentals - <?=$title?></title>
-        </head>
-        <body>
-            <h1><?php echo $message ?></h1>
-        </body>
-    </html>
+$caseSensitive = "I am unique.";
+$CaseSensitive = "I’m different.";
+echo $caseSensitive;
+echo $CaseSensitive;
+?>
