c8482ae460ee81a70178a6a3a2b13e9b20663b6b
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 05:48:26 -0700

    c

--- web/index.php
+++ web/index.php
@@ -3,6 +3,6 @@
     $sampleString = 'A $currency saved is a $currency earned.';
     echo $sampleString;
 
-    $mics = 'St. Patrick\'s Day\n';
+    $misc = 'St. Patrick\'s Day\n';
     echo $misc;
     ?>
