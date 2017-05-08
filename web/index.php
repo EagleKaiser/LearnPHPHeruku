70de51f16bdef1600fd0989497ccf7909ca7263c
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 07:49:25 -0700

    c

--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@
     $replaced = str_replace("be", "know", $quote)."<br>";
     echo $replaced;
     $replaced = str_replace("know", "be", $replaced, $count)."<br>";
-    echo $replaced."<br>number of replaces= ".$count;
+    echo $replaced."number of replaces= ".$count;
     ?>
