56f601b31946a8c1d4d7a23f8b300c16dad969bf
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 06:04:20 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,8 +1,12 @@
 <?php
-    $currency = 'penny';
-    $sampleString = 'A $currency saved is a $currency earned.';
-    echo $sampleString;
-    echo "<br>";
-    $misc = 'St. Patrick\'s Day\n';
+    $currency = penny;
+    $sampleString = "A $currency saved is a $currency earned.";
+    echo $sampleString."<br>;
+
+    $var = 2;
+    echo "{$var}nd place<br>;
+    echo $var."nd place<br>;
+
+    $misc = "St. Patrick\"s <br>Day\n";
     echo $misc;
     ?>
