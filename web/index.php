93f6b0b132fd92851f325036f379eec15e9d69b3
EagleKaiser <eagle2117652@outlook.com>
2017-05-08 07:47:17 -0700

    c

--- web/index.php
+++ web/index.php
@@ -2,6 +2,6 @@
     $quote = "To be or not to be, that is the question.";
     $replaced = str_replace("be", "know", $quote)."<br>";
     echo $replaced;
-    $replaced = str_replace("be", "know", $quote, $count)."<br>";
+    $replaced = str_replace("be", "know", $replaced, $count)."<br>";
     echo $replaced."<br>number of replaces= ".$count;
     ?>
