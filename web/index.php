cbcf64ab945054b2ff4a5c7370a156a065eec59d
EagleKaiser <eagle2117652@outlook.com>
2017-05-10 22:05:56 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,10 +1,9 @@
 <?php
-    $firstType = array("charles", "hello", "world", "Array");
-    $secondType = ["charles", "hello", "world", "Array"];
-    $allValues = [10, 1.3, "hello", true];
-    print_r($firstType);
-    echo "<br>";
-    print_r($secondType);
-    echo "<br>";
-    print_r($allValues);
+    $authors = array(
+    "quarky" => "Charles Dickens",
+    10 => "Jane",
+    "poetic" => "William",
+    "Mark Twain"
+    );
+    print_r($authors);
     ?>
