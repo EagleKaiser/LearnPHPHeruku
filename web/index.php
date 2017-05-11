20a099bb44d5c5436554c70923aafcd6e1928fea
EagleKaiser <eagle2117652@outlook.com>
2017-05-11 01:35:29 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
  <?php
     $authors = array("charles", "hello", "world", "Array");
     $lastValue = array_pop($authors);
-    echo $lastValue;
+    echo $lastValue."<br>";
     print_r($authors);
+    //output:
+    //ArrayArray ( [0] => charles [1] => hello [2] => world ) 
     ?>
