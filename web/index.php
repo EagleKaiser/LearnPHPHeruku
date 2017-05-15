8ad39af2b82e63064f8cd67ea55ea9ce06a8e859
EagleKaiser <eagle2117652@outlook.com>
2017-05-15 06:22:58 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
 <?php
     class Person
     {
+        const AVG_LIFE_SPAN = 79;
         public $firstName = "Samel";
         public $lastName = "Clemens";
         public $yearBorn = "1988";
     }
     $myObject = new Person();
-    echo $myObject->firstName."<br>";
-    $myObject->firstName = "S.L";
-    echo $myObject->firstName;
+    echo $myObject::AVG_LIFE_SPAN."<br>";
+    echo Person::AVG_LIFE_SPAN;
     ?>
