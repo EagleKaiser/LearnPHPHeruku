77a7316c89bfda4da0e7bf8671ff2f1144436daf
EagleKaiser <eagle2117652@outlook.com>
2017-05-15 04:33:55 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,15 +1,11 @@
  <?php
-    $first = array("charles", "hello", "world", "Array");
     $second = array(
     "quarky" => "Charles Dickens",
     "brilliant" => "Jane",
     "poetic" => "William",
     "Mark Twain"
     );
-    asort($first);
-    print_r($first);
-    echo "<br>";
-    asort($second);
+    ksort($second);
     print_r($second);
-
     ?>
+    
