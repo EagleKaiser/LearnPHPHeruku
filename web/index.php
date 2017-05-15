d6b857a23cb8930644178a33fff80d0a67ea42d8
EagleKaiser <eagle2117652@outlook.com>
2017-05-15 06:54:56 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,12 +1,21 @@
 <?php
-    class Person
+     class Person
     {
         const AVG_LIFE_SPAN = 79;
         public $firstName = "Samel";
         public $lastName = "Clemens";
         public $yearBorn = "1988";
+
+        public function getFirstName()
+        {
+            return $this->firstName;
+        }
+        public function setFirstName($tempName)
+        {
+            $this->firstName = $tempName;
+        }
     }
     $myObject = new Person();
-    echo $myObject::AVG_LIFE_SPAN."<br>";
-    echo Person::AVG_LIFE_SPAN;
+    $myObject->setFirstName("Sam");
+    echo $myObject->getFirstName();
     ?>
