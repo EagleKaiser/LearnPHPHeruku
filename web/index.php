568b68ea12630740b2dbf4026f9506cae525e96d
EagleKaiser <eagle2117652@outlook.com>
2017-05-15 05:36:29 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,18 +1,16 @@
 <?php
-    $first = array("charles", "hello", "world", "Array");
-    echo count($first). "<br>";
-    //output: 4
-    $authors2 = [
-            "Male" => array(
-                "Charles" => array("Christmas Carol", "Oliver Twist"),
-                "William" => array("Romeo & Juliet", "Richard III"),
-                "Mark Twain" => Array("Tom Sawyer", "Huck Finn")
-                ),
-            "Female" => array(
-                "L. M. Montgomery" => array("Anne of Green", "Anne of Avolea"),
-                "Louisa May" => array("Litle Women")
-                )
-            ];
-    echo count($authors2)."<br>";
-    echo count($authors2, 1);  
+    $second = array(
+    "quarky" => "Charles Dickens",
+    "brilliant" => "Jane",
+    "poetic" => "William",
+    "Mark Twain"
+    );
+    foreach($authors as $val)
+    {
+        echo $val."<br>";
+    }
+    foreach($authors as $key => $val)
+    {
+        echo $val."(".$key.")<br>";
+    }
     ?>
