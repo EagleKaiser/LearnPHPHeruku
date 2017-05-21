20d9247b68c3a5ad147c5a0541c143a62d7e0218
EagleKaiser <eagle2117652@outlook.com>
2017-05-21 05:30:05 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,11 +1,13 @@
-$readingIsFun = true;
+
+    <?php
+    $readingIsFun = true;
     $authors = ["charles", "jane", "William"];
     $count = count($authors);
     
-    if($count > 0 :
+    if($count > 0) :
         echo "there is a total of ".$count." authors<br>";
     else :
-        echo "there are no authors<br>"
+        echo "there are no authors<br>";
     endif;
 
     $i = 0;
