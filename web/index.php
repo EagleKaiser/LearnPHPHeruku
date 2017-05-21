e39eb5a4c3af72e0f52fe8b71174f3ea3793fbe5
EagleKaiser <eagle2117652@outlook.com>
2017-05-21 05:04:04 -0700

    c

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
     $authors = ["charles", "jane", "William"];
     $count = count($authors);
     $outcome = $count ? $count : "Count unavailable";
-    echo "<br>";
+    echo $outcome."<br>";
     $outcome = $count ?? "count unavailable";
     echo $outcome;
     ?>
