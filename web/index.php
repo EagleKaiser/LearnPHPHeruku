53784de47ea8acfe6baf9b34c992dabeead159d6
EagleKaiser <eagle2117652@outlook.com>
2017-05-21 05:27:20 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,8 +1,20 @@
-<?php
+$readingIsFun = true;
     $authors = ["charles", "jane", "William"];
     $count = count($authors);
-    $outcome = $count ? $count : "Count unavailable";
-    echo $outcome."<br>";
-    $outcome = $count ?? "count unavailable";
-    echo $outcome;
+    
+    if($count > 0 :
+        echo "there is a total of ".$count." authors<br>";
+    else :
+        echo "there are no authors<br>"
+    endif;
+
+    $i = 0;
+    while($readingIsFun) :   
+        echo "reading is fun"."<br>";
+        $readingIsFun = false;
+    endwhile;
+
+    for($i = 0; $i <5; $i++) :
+        echo "Reading is fun";
+    endfor
     ?>
