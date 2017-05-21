5ffbc04658c39d37501f53d3babaef30d4847629
EagleKaiser <eagle2117652@outlook.com>
2017-05-21 04:09:24 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,9 +1,12 @@
 <?php
-    $increment = 2;
-    echo $increment++."<br>";
-    echo $increment."<br>";
-    echo ++$increment."<br>";    
-    echo $increment--."<br>";
-    echo $increment."<br>";
-    echo --$increment."<br>"; 
+    $var1 = (6 < 7);
+    $var2 = (8 ==8);
+
+    $var3 = true;
+    $var4 = false;
+
+    var_dump($var1);
+    var_dump($var2);
+    var_dump($var3 && $var4);
+
     ?>
