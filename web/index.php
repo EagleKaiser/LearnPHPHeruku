a1dbd270cf78430fe60a2caf5d4655638512f144
EagleKaiser <eagle2117652@outlook.com>
2017-05-21 03:43:07 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,15 +1,6 @@
 <?php
-    class Foo
-    {
-        public static $centuryPopular = "19";
-        public static function getCentury()
-        {
-            return self::$centuryPopular;
-        }
-    }
-    echo Foo::$centuryPopular;
-    echo "<br>";
-    echo Foo::getCentury();
-
-
+    $increment = 2;
+    echo $increment++."<br>";
+    echo $increment."<br>";
+    echo ++$increment."<br>";    
     ?>
