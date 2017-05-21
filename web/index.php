060f8ca6a4a0e167eb76522f2059fce6951c8fa2
EagleKaiser <eagle2117652@outlook.com>
2017-05-21 04:49:05 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,13 +1,7 @@
 <?php
-    $authors = [];
+    $authors = ["charles", "jane", "William"];
     $count = count($authors);
-    switch($count)
-    {
-        case 0:
-            echo "there are no authors";
-        case 1:
-            echo "there is 1 authors";
-        default:
-            echo "there is a total of ".$count." authors";
-    }
+
+    $outcome = ($count > 0) ? "Author Total: ".$count : "noAuthors";
+    echo $outcome;
     ?>
