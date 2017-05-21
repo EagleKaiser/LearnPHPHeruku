2a0afad8e43452f5de42bb21f9a0b37b0129a1c5
EagleKaiser <eagle2117652@outlook.com>
2017-05-21 01:17:53 -0700

    c

--- web/index.php
+++ web/index.php
@@ -1,51 +1,13 @@
 <?php
-    class Person
+    class Foo
     {
-        const AVG_LIFE_SPAN = 79;
-        private $firstName;
-        private $lastNamens;
-        private $yearBorn;
-        function __construct($tempFirst = "", $tempLast = "", $tempBorn = "")
+        public static $centuryPopular = "19";
+        public static function getCentury()
         {
-            echo "Person Constructor<br>";
-            $this->firstName = $tempFirst;
-            $this->lastName = $tempLast;
-            $this->yearBorn = $tempBorn;
-        }
-        private function getFirstName()
-        {
-            return $this->firstName;
-        }
-        private function setFirstName($tempName)
-        {
-            $this->firstName = $tempName;
-        }
-        private function getFullName()
-        {
-            echo "Person->getFullName()<br>";
-            return $this->firstName." ".$this->lastName."<br>";
+            return self::$centuryPopular;
         }
     }
-    class Author extends Person
-    {
-        protected $penName = "Mark Twain";
-        public function getPenName()
-        {
-            return $this->penName."<br>";
-        }
-        public function getFullName()
-        {
-            echo "Author->getCompleteName()<br>";
-            //access protected properties through this public method
-            return $this->firstName." ".$this->lastName."a.k.a. ".$this->penName."<br>";
-        }
-    }
-    $newAuthor = new Author("Samuel Langhorne", "Clemns", 1899);
-    echo $newAuthor->getFullName();
-    //output
-    //Person Constructor
-    //Author->getCompleteNme()
-    //Samuel Langhorne Clemnsa.k.a. Mark Twain    
-    ?>
-
+    echo Foo::$centuryPopular;
     
+
+    >?
